1990067542b0e9ed73c2ab388514d87dcc7d134e
Joldasbayeva <joldasbaevaaqmaral@gmail.com>
2023-12-16 15:29:28 +0500

    finally all correct

---
+++ my_first_scraper.py
@@ -0,0 +1,47 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+
+def request_github_trending(url):
+    return requests.get(url)
+
+
+
+def extract(page):
+    soup = BeautifulSoup(page.text, "html.parser")
+    return soup.find_all("article")
+
+
+
+
+def transform(html_repos):
+    join_in = []
+    for num in html_repos:
+
+        repository_name = ''.join(num.select_one('h2.h3.lh-condensed').text.split())
+
+        number_stars = ' '.join(num.select_one('span.d-inline-block.float-sm-right').text.split())
+
+        developer_name = num.select_one('img.avatar.mb-1.avatar-user')['alt']
+
+        join_in.append({'developer': developer_name, 'repository_name': repository_name, 'nbr_stars': number_stars})
+    return join_in
+
+
+
+def format(repositories_data):
+    join_in = ["Developer, Repository Name, Number of Stars"]
+
+
+
+    for respon in repositories_data:
+        num = [respon['developer'], respon['repository_name'], respon['nbr_stars']]
+        join_in.append(', '.join(num))
+    return "\n".join(join_in)
+
+
+trending = request_github_trending("https://github.com/trending")
+extracted = extract(trending)
+answer = transform(extracted)
+print(answer)
