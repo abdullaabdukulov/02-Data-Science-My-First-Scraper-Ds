16ec8157ba0b960c1ee1e4a5c7e0edf080c870b4
boburergashew <153498795+boburergashew@users.noreply.github.com>
2024-01-13 23:18:08 +0500

    Create my_first_scraper.py
    Finished Successfully

---
+++ my_first_scraper.py
@@ -0,0 +1,35 @@
+import requests
+from bs4 import BeautifulSoup
+
+def request_github_trending(url):
+    return requests.get(url)
+
+def extract(page):
+    soup = BeautifulSoup(page.text, "html.parser")
+    return soup.find_all("article")
+
+def transform(html_repos):
+    join_in = []
+    for num in html_repos:
+        repository_name = ''.join(num.select_one('h2.h3.lh-condensed').text.split())
+        number_stars = ' '.join(num.select_one('span.d-inline-block.float-sm-right').text.split())
+        developer_name = num.select_one('img.avatar.mb-1.avatar-user')['alt']
+        join_in.append({'developer': developer_name, 'repository_name': repository_name, 'nbr_stars': number_stars})
+    return join_in
+
+def format(repositories_data):
+    join_in = ["Developer, Repository Name, Number of Stars"]
+    for respon in repositories_data:
+        num = [respon['developer'], respon['repository_name'], respon['nbr_stars']]
+        join_in.append(', '.join(num))
+    return "\n".join(join_in)
+
+def run():
+    url = "https://github.com/trending"
+    page = request_github_trending(url)
+    html_repos = extract(page)
+    repositories_data = transform(html_repos)
+    print(repositories_data)
+    # print(format(repositories_data))
+    
+run()
